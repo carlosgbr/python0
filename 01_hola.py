0f02303fa82b92877fb85ec0a5b761fd2e10a92a
carlosgbr <carlosgbr@gmail.com>
2019-11-10 17:37:23 -0600

    Create 01_hola.py

---
+++ 01_hola.py
@@ -0,0 +1,10 @@
+'''
+ Programa: 01_hola.py
+ Hola Mundo en Pyton, para ilustrar aspectos básicos en un programa en Python
+ Por http://about.me/carlosgbr
+ Fecha: 10/11/19
+ Versión 1
+ Para Python 3.0 y superior
+'''
+
+print ("Hola Mundo!") #Si añades un espacio antes de la p, tendrás un error
