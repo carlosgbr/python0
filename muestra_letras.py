5acc7fa244523bc6a350297bdce5eef9af9648d9
carlosgbr <carlosgbr@gmail.com>
2020-04-26 01:36:08 -0500

    Create muestra_letras.py

---
+++ muestra_letras.py
@@ -0,0 +1,16 @@
+""" Loops: muestra_letras.py
+  Por about.me/carlosgbr
+  Versión 1
+  Para Python 3.0 y superior
+  Se ilustra el bucle for para recorrer cadenas de texto
+"""
+"""
+ENUNCIADO: La función muestra_letras debe imprimir cada letra de una palabra 
+en una línea separada. Complete los espacios en blanco para que eso suceda.
+"""
+def muestra_letras(palabra):
+	for letra in palabra:
+		print(letra)
+
+muestra_letras("Hola")
+# Debe imprimir una letra por línea
